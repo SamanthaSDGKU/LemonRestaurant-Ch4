3342333b7c0ec86e70957795d6004b2b9fbe5e47
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-10-23 19:53:34 -0700

    Data collection

---
+++ LemonRestaurant-Ch4/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  LemonRestaurant-Ch4
+//
+//  Created by Samantha Jimenez on 23/10/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
